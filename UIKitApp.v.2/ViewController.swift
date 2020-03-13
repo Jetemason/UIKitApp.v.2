70620661fb13e6444ac3e1ce4ce80d41517fe976
Igor Ilyin <53910111+Jetemason@users.noreply.github.com>
2020-03-13 18:32:13 +0500

    Initial Commit

---
+++ UIKitApp.v.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKitApp.v.2
+//
+//  Created by Игорь Ильин on 13.03.2020.
+//  Copyright © 2020 Igor Ilyin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
